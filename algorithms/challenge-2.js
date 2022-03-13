8be7317be831a9e6d8bc9a99027bf84a4a8f2f84
Anthonyponplex <mayakianthony@yahoo.com>
2022-03-13 18:17:16 +0100

    Implemented my assignment

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,24 @@
 function transpose(arr) {}
+function transpose(arr){
+let row = arr.lenght;
+let col = arr[0].lenght;
+let result = [];
+if ((row >=1 && row<=10) && (col >=1 && col<=10)){
+    for (let i=0; i< col; i==){
+        let subArray =[];
+        for(let j=0; j< row; j==){
+            subArray.push(arr[j][i]);
+        }
+        result.push(subArray);
+ 
+    }
+   return result;
+ 
+}
 
-module.exports = transpose
+console.log(transpose([
+    [5, 12, 17. 9, 3]
+    [13, 4, 8, 14, 1]
+    [9, 6,3, 7, 21]
+]))
+
--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,3 +1,23 @@
-function binaryReversal(value) {}
+function binaryReversal(value) {
+     let result = [0,0,0,0,0,0,0,0];
+     let index =0;
+     while (input >0){
+         if(index >result.length -1){
+         result.push(0,0,0,0,0,0,0,0);
+        }
+         result[index] = input % 2;
+         input =(input - result [index]) / 2;
+         index++
+     }
+     let reversed = 0;
+     let letOfResult = result.length - 1;
+    for (let i=0; i<=letOfResult; i++){
+         reversed+=
+         result[i]*Math.pow(2, (letOfResult- i));
+     }
+     return reversed;
+    
 
+console.log(binaryReversal("47"));
+console.log(binaryReversal (4567));
 module.exports = binaryReversal
